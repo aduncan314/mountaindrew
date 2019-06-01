254fd2bc5bc4a1251cd68449c3ca95c81f5b6de3
aduncan314 <bluesnpolitics@yahoo.com>
2019-06-01 16:54:49 -0400

    move base.html to be for all apps

--- landing/views.py
+++ landing/views.py
@@ -7,8 +7,8 @@ def index(request):
         'my_name': 'Drew',
         'simple_intro': "This is my personal site. It's not too nice, but it has character"
     }
-    return render(request, 'index.html', context=content_dict)
+    return render(request, 'landing/index.html', context=content_dict)
 
 
 def cv(request):
-    return render(request, 'cv.html', context=None)
+    return render(request, 'landing/cv.html', context=None)
