8688f15044e191f3a4a4be91deb51679e2cb3307
aduncan314 <mountaindrew.duncan@gmail.com>
2019-09-02 22:31:07 -0400

    working markdown filter; add page name change for child templates

--- blog/templatetags/pygment_filter.py
+++ blog/templatetags/pygment_filter.py
@@ -1,24 +1,32 @@
 import mistune
+import logging
 from django import template
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import get_lexer_by_name
 
+logger = logging.getLogger('django')
 register = template.Library()
 
 
 class HighlightRenderer(mistune.Renderer):
+    """
+    Mistune renderer use by Markdown class. Taken from mistune site example with better error handling
+    """
     def block_code(self, code, lang=None):
-        if lang is None:
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        except Exception as e:
+            # Log error but don't worry
+            logger.error(e)
             return '\n<pre><code>%s</code></pre>\n' % \
                    mistune.escape(code)
-        lexer = get_lexer_by_name(lang, stripall=True)
-        formatter = HtmlFormatter()
-        return highlight(code, lexer, formatter)
 
 
 @register.filter
 def markdown(value):
     renderer = HighlightRenderer()
-    markdown = mistune.Markdown(renderer=renderer)
-    return markdown(value)
+    md = mistune.Markdown(renderer=renderer)
+    return md(value)
